d98d0c9d201491d35c91859ca4cfb33ea6125306
djghis <djghis@hotmail.com>
2020-03-23 16:01:29 +0000

    add test passing

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+//    public Calculator(int number1, int number2){
+//        this.number1 = number1;
+//        this.number2 = number2;
+//    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
