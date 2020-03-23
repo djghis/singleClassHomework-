76057b30eff47d856c1510a73804872b59b136f7
djghis <djghis@hotmail.com>
2020-03-23 16:46:30 +0000

    printer has sheets passed

---
+++ src/main/java/Printer.java
@@ -0,0 +1,12 @@
+public class Printer {
+
+    private int sheets;
+
+    public  Printer(int sheets){
+        this.sheets = sheets;
+    }
+
+    public int hasSheets(){
+        return this.sheets;
+    }
+}
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer (50);
+    }
+
+    @Test
+    public void hasSheets(){
+        assertEquals(50, printer.hasSheets());
+    }
+}
