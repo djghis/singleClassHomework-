78a7ee9358f79f8ed07d4082a5152ed82025d565
djghis <djghis@hotmail.com>
2020-03-23 19:50:37 +0000

    toner decreasing passing but can't get the sheets and toner to decrease in 1 method

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,9 +1,11 @@
 public class Printer {
 
     private int sheets;
+    private int toner;
 
-    public Printer(int sheets) {
+    public Printer(int sheets, int toner) {
         this.sheets = sheets;
+        this.toner = toner;
     }
 
     public int hasSheets() {
@@ -13,9 +15,15 @@ public class Printer {
 
     public String print(int pages, int copies) {
         if (this.sheets >= pages * copies) {
-            return String.valueOf(this.sheets -= pages * copies);
+            return String.valueOf(this.toner -= pages * copies);
         } else {
             return "Not enough paper";
         }
     }
+
+    public int hasToner() {
+        return this.toner;
+    }
+
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,7 +9,7 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        printer = new Printer (50);
+        printer = new Printer (50, 500);
     }
 
     @Test
@@ -20,10 +20,23 @@ public class PrinterTest {
     @Test
     public void printHasEnoughPaper() {
         assertEquals("44", printer.print(2,3));
+        assertEquals(44, printer.hasSheets());
     }
+
 
     @Test
     public void printHasNotEnoughPaper() {
         assertEquals("Not enough paper", printer.print(10,10));
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(500, printer.hasToner());
+    }
+
+    @Test
+    public void tonerIsDecreasing(){
+        printer.print(2,3);
+        assertEquals(494, printer.hasToner());
     }
 }
