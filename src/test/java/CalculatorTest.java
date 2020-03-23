db584ccdb7bf26b0707803f84b2d528874c668a8
djghis <djghis@hotmail.com>
2020-03-23 16:12:15 +0000

    divide test pass

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -26,4 +26,9 @@ public class CalculatorTest {
     public void canMultiply(){
         assertEquals(6, calculator.multiply(2,3));
     }
+
+    @Test
+    public void canDivide(){
+        assertEquals(1.67, calculator.divide(5.00,3.00), 0.01);
+    }
 }
